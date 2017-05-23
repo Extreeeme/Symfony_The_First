a90b18480b687e171338a1ac949cdff1aa7d7a3f
Extreeeme <martindcontact@gmail.com>
2017-05-23 11:14:28 +0200

    Avancement

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -4,18 +4,34 @@ namespace OC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
 
 class AdvertController extends Controller
 {
+
   public function indexAction()
   {
-    $content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'Martin'));
-    return new Response($content);
+    $url = $this->get('router')->generate(
+      'oc_platform_home',
+      array(),
+      UrlGeneratorInterface::ABSOLUTE_URL
+    );
+    return new Response("L'URL de l'annonce d'id 5 est : $url");
   }
 
-  public function byeAction()
+  public function viewAction($id)
   {
-    $content = $this->get('templating')->render('OCPlatformBundle:Advert:bye.html.twig', array('nom' => 'World'));
-    return new Response($content);
+    // $content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'Martin'));
+    return new Response('La variable donnée est '.$id);
+  }
+
+  public function viewSlugAction($year, $slug, $format)
+  {
+    // $content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'Martin'));
+    return new Response(
+            "On pourrait afficher l'annonce correspondant au
+            slug '".$slug."', créée en ".$year." et au format ".$format."."
+        );
   }
 }
